0210f92c67640211beb9c286f0c668aa22c153b3
flycal6 <brianraythomas@gmail.com>
2017-03-14 15:23:15 +0300

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+    public static void main( String[] args ) {
+        System.out.println( "I, Brian Thomas, am determined to learn how to code." );
+        System.out.println( "Today's date is March 17th, 2017" );
+    }
+}

8a51b1ec256fc692c707490705582441ff242570
flycal6 <brianraythomas@gmail.com>
2017-03-25 10:29:30 +0300

    added exercise 46

---
+++ NestingLoops.java
@@ -0,0 +1,28 @@
+public class NestingLoops {
+    public static void main( String[] args ) {
+        // this is #1 = I'll call it "CN"
+        for (int n=1; n <= 3; n++) {
+            for (char c='A'; c <= 'E'; c++) {
+                System.out.println( c + " " + n);
+            }
+        }
+
+        System.out.println("\n");
+
+        // this is #2 - I'll call it "AB"
+        for (int a=1; a <= 3; a++) {
+            for (int b=1; b <= 3; b++) {
+                System.out.print("(" + a + "," + b + ")");
+            }
+            // * You will add a line of code here.
+            System.out.println("Inner loop end.");
+        }
+
+        System.out.println("\n");
+    }
+}
+// SD #1  The inner var changes faster, 3 times for each outer loop change
+// SD #2  The letters now cycle through A-E before the int iterates.
+//          Total lines of output is unchanged
+// SD #3  The AB loop now prints each number pair on it's own line
+// SD #4  The println get printed once the inner loop condition is no longer true.  
